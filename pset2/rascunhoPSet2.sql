6ea94a078e33a400b824699c0b2653844dc30a74
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-04 10:09:03 -0300

    Update rascunhoPSet2.sql

--- pset2/rascunhoPSet2.sql
+++ pset2/rascunhoPSet2.sql
@@ -1,8 +1,8 @@
 
 /* rascunho questão 6 */
 select *
-case when dts.sexo = 'M' then 'Masculino'
-when dts.sexo = 'F' then 'Feminino'
+case when sexo = 'M' then 'Masculino'
+when sexo = 'F' then 'Feminino'
 end as sexo_dependente
 from (select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente
 from funcionario as f, departamento as d, dependente as dts
