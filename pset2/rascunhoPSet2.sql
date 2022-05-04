64e197e0257984827d2a949ff57be71bf438f3e6
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-04 11:04:12 -0300

    Update rascunhoPSet2.sql

--- pset2/rascunhoPSet2.sql
+++ pset2/rascunhoPSet2.sql
@@ -30,4 +30,7 @@ where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departame
 select *
 from (select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente
 from funcionario as f, departamento as d, dependente as dts
-where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departamento) as formulario;
+where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departamento) as formulario
+case dts 
+when sexo = 'M' then sexo = 'Masculino'
+end;
