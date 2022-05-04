be845e6e4a751798259c7b203f65cd345b27e663
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-04 09:14:51 -0300

    Create rascunhoPSet2.sql

---
+++ pset2/rascunhoPSet2.sql
@@ -0,0 +1,3 @@
+select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente
+from funcionario as f, departamento as d, dependente as dts
+where dts.cpf_funcionario = f.cpf;
