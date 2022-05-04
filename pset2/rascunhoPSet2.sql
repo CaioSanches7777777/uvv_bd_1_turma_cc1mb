6564a4ace1d1b641caf08da99f19733d9f2cae6f
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-04 09:47:17 -0300

    Update rascunhoPSet2.sql

--- pset2/rascunhoPSet2.sql
+++ pset2/rascunhoPSet2.sql
@@ -3,5 +3,4 @@
 select *
 from (select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente
 from funcionario as f, departamento as d, dependente as dts
-inner join funcionario as f2 on d.numero_departamento=f2.numero_departamento
 where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departamento) as formulario;
