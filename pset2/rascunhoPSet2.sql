ea0c7734dc7a2e2bcdc32eb80d4a2e7c8af251ea
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-04 12:44:02 -0300

    Update rascunhoPSet2.sql

--- pset2/rascunhoPSet2.sql
+++ pset2/rascunhoPSet2.sql
@@ -28,12 +28,13 @@ where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departame
 
 /* rascunho questão 6 */
 select *
-from (select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente
+from (select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente,
+case dts.sexo
+      when 'M' then 'Masculino'
+      when 'F' then 'Feminino'
+end as sexo_dependente
 from funcionario as f, departamento as d, dependente as dts
-where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departamento) as formulario
-case 
-when sexo = 'M' then 'Masculino'
-end;
+where dts.cpf_funcionario = f.cpf and d.numero_departamento = f.numero_departamento) as formulario;
 
 
 
