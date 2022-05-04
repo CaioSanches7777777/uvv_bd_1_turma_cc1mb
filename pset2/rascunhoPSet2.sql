546e482833a87b22612bc58a04182a325d16be5e
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-04 09:41:21 -0300

    Update rascunhoPSet2.sql

--- pset2/rascunhoPSet2.sql
+++ pset2/rascunhoPSet2.sql
@@ -2,5 +2,5 @@
 /* rascunho questão 6 */
 select d.nome_departamento, f.primeiro_nome as primeiro_nome_funcionario, f.nome_meio as nome_meio_funcionario, f.ultimo_nome as ultimo_nome_funcionario, dts.nome_dependente, f.nome_meio as nome_meio_dependente, f.ultimo_nome as ultimo_nome_dependente, dts.sexo, year(curdate()) - year(dts.data_nascimento) as idade_dependente
 from funcionario as f, departamento as d, dependente as dts
-inner join f on d.numero_departamento=f.numero_departamento
+inner join funcionario as f on d.numero_departamento=f.numero_departamento
 where dts.cpf_funcionario = f.cpf;
