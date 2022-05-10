30fd979fa8e8885828076d63cdabf465835a269b
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-05-10 09:25:34 -0300

    Update scriptPset2.sql

--- pset2/scriptPset2.sql
+++ pset2/scriptPset2.sql
@@ -102,34 +102,13 @@ select * from relatorio_8;
 
 -- 9)O comando a baixo seleciona o nome dos departamentos, o nome e o numero de cada projeto de cada departamento e a soma do numero de horas investidas em cada projeto.
 
-CREATE VIEW relatorio_9 as
-select nome_departamento, numero_e_nome_projeto, SUM(horas)
-from relatorio_8
-where numero_e_nome_projeto = '(Nº1)ProdutoX'
-union
-select nome_departamento, numero_e_nome_projeto, SUM(horas)
-from relatorio_8
-where numero_e_nome_projeto = '(Nº2)ProdutoY'
-union
-select nome_departamento, numero_e_nome_projeto, SUM(horas)
-from relatorio_8
-where numero_e_nome_projeto = '(Nº3)ProdutoZ'
-union
-select nome_departamento, numero_e_nome_projeto, SUM(horas)
-from relatorio_8
-where numero_e_nome_projeto = '(Nº10)Informatização'
-union
-select nome_departamento, numero_e_nome_projeto, SUM(horas)
-from relatorio_8
-where numero_e_nome_projeto = '(Nº20)Reorganização'
-union
-select nome_departamento, numero_e_nome_projeto, SUM(horas)
-from relatorio_8
-where numero_e_nome_projeto = '(Nº30)Novosbenefícios';
 
-/*Favor ignorar o erro (que ocorre quando a view relatorio_9 já foi criada) acima, essa view será usada como abreviação da questão 9*/
+select nome_departamento, numero_e_nome_projeto, SUM(horas)
+from relatorio_8
+group by numero_e_nome_projeto;
 
-select * from relatorio_9;
+
+
 
 
 
