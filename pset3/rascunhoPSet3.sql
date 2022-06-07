d3a37345335261d393a00ffcb4d9bd4b8339f98c
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-06-07 15:23:39 -0300

    Create rascunhoPSet3.sql

---
+++ pset3/rascunhoPSet3.sql
@@ -0,0 +1,114 @@
+--rascunho
+with classificacoes as (
+	select codigo, nome, codigo_pai
+	from classificacao
+	where codigo_Pai is null)
+	, meio_termo as (
+	select cl.codigo, cl.nome, cl.codigo_pai
+	from classificacao as cl)
+	, produtos as (	
+	select cls.codigo, cls.nome, cls.codigo_pai
+	from classificacao as cls
+	where cls.codigo_pai is not null)
+select classificacoes.codigo, classificacoes.nome, classificacoes.codigo_Pai
+from classificacoes
+union 
+select produtos.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome), produtos.codigo_Pai
+from produtos 
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+union 
+select produtos.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome , ' --> ', produtos.nome), produtos.codigo_Pai
+from produtos
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+order by nome
+;
+
+/*
+with classificacoes as (
+	select codigo, nome, codigo_pai
+	from classificacao
+	where codigo_Pai is null)
+	, meio_termo as (
+	select cl.codigo, cl.nome, cl.codigo_pai
+	from classificacao as cl)
+	, produtos as (	
+	select cls.codigo, cls.nome, cls.codigo_pai
+	from classificacao as cls
+	where cls.codigo_pai is not null)
+select produtos.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome , ' --> ', produtos.nome), produtos.codigo_Pai
+from produtos
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+order by classificacoes.nome
+;*/
+
+--Meio Proximo
+with classificacoes as (
+	select codigo, nome, codigo_pai
+	from classificacao
+	where codigo_Pai is null)
+	, meio_termo as (
+	select cl.codigo, cl.nome, cl.codigo_pai
+	from classificacao as cl)
+	, produtos as (	
+	select cls.codigo, cls.nome, cls.codigo_pai
+	from classificacao as cls
+	where cls.codigo_pai is not null)
+select classificacoes.codigo, classificacoes.nome, classificacoes.codigo_Pai
+from classificacoes
+union 
+select produtos.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome), produtos.codigo_Pai
+from produtos 
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+union 
+select produtos.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome , ' --> ', produtos.nome), produtos.codigo_Pai
+from produtos
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+order by nome
+;
+
+
+
+
+--Mais Proximo
+with classificacoes as (
+	select codigo, nome, codigo_pai
+	from classificacao
+	where codigo_Pai is null)
+	
+	, meio_termo as (
+	select cl.codigo, cl.nome, cl.codigo_pai
+	from classificacao as cl)
+	
+	, produtos as (	
+	select cls.codigo, cls.nome, cls.codigo_pai
+	from classificacao as cls
+	where cls.codigo_pai is not null)
+	
+select classificacoes.codigo, classificacoes.nome, classificacoes.codigo_Pai
+from classificacoes
+union 
+
+select mt.codigo, concat(classificacoes.nome, ' --> ', mt.nome), mt.codigo_Pai
+from produtos
+inner join meio_termo mt on mt.codigo_pai = produtos.codigo 
+inner join classificacoes on classificacoes.codigo = mt.codigo_pai
+union
+
+select meio_termo.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome), meio_termo.codigo_Pai
+from produtos 
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+inner join meio_termo mt on mt.codigo = meio_termo.codigo_Pai
+union 
+
+select produtos.codigo, concat(classificacoes.nome, ' --> ', meio_termo.nome , ' --> ', produtos.nome), produtos.codigo_Pai
+from produtos
+inner join meio_termo on meio_termo.codigo = produtos.codigo_pai
+inner join classificacoes on classificacoes.codigo = meio_termo.codigo_pai
+order by nome
+;
