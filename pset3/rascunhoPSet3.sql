ccf4745c493a6ca449327bd162801aaec587545a
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-06-08 16:12:04 -0300

    Update rascunhoPSet3.sql
    Modelo que finalmente deu certo

--- pset3/rascunhoPSet3.sql
+++ pset3/rascunhoPSet3.sql
@@ -1,3 +1,21 @@
+/* ******** Modelo que finalmente deu certo ********** */
+
+with recursive classificacao_P as (
+	select codigo, concat(nome) as nome, codigo_pai
+	from classificacao
+	where codigo_pai is null
+	union all
+	select cls.codigo, concat(pr.nome, ' --> ', cls.nome), cls.codigo_pai
+	from classificacao as cls
+	inner join classificacao_P as pr on pr.codigo = cls.codigo_pai
+	where cls.codigo_Pai is not null)
+select *
+from classificacao_P 
+order by classificacao_P.nome
+;
+
+
+
 --rascunho
 
 /*Modelo simples baseado nos links no arquivo de texto no PSet3*/
