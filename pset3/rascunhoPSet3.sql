fa43868363c1df2b313d18edc67c4be0fc2e4a08
CaioSanches7777777 <102621072+CaioSanches7777777@users.noreply.github.com>
2022-06-08 15:31:20 -0300

    Update rascunhoPSet3.sql
    Rascunhos com o mesmo output.

--- pset3/rascunhoPSet3.sql
+++ pset3/rascunhoPSet3.sql
@@ -164,3 +164,52 @@ inner join classificacao on classificacao.codigo_Pai = cls.codigo
 left join classificacao clss on clss.codigo_Pai = classificacao.codigo
 order by nome
 ;
+
+
+/* Mesmo output
+
+with recursive classificacao_P as (
+	select codigo,
+	nome,
+	codigo_pai
+	from classificacao
+	where codigo_pai is null
+		union
+	select cls.codigo,
+	cls.nome,
+	cls.codigo_pai
+	from classificacao as cls
+	where cls.codigo_pai is not null
+	union
+	select clss.codigo,
+	clss.nome,
+	clss.codigo_pai
+	from classificacao as clss
+	inner join classificacao as cls on cls.codigo = cls.codigo_pai
+	inner join classificacao_P on classificacao_P.codigo = cls.codigo_pai
+	where cls.codigo_pai is not null)
+select cls.codigo, 
+case cls.nome
+	when cls.nome then concat(classificacao_P.nome, case classificacao_P.nome when classificacao_P.nome then concat(' --> ', cls.nome, case cls.nome when cls.nome then concat(' --> ',classificacao.nome) else '' end) else '' end)
+	end as nome,
+cls.codigo_Pai
+from classificacao_P
+inner join classificacao cls on cls.codigo_Pai = classificacao_P.codigo
+inner join classificacao on classificacao.codigo_Pai = cls.codigo
+left join classificacao clss on clss.codigo_Pai = classificacao.codigo
+order by nome
+;
+
+--
+
+select cls.codigo,
+concat(clss.nome, case when clss.codigo = cls.codigo_Pai then concat(' --> ', cls.nome, case when cls.codigo = cla.codigo_Pai then concat(' --> ', cla.nome) else '' end)end),
+cls.codigo_Pai
+from classificacao as clss
+inner join classificacao as cls on cls.codigo_Pai = clss.codigo
+inner join classificacao as cla on cla.codigo_Pai = cls.codigo
+where cls.codigo_Pai = clss.codigo
+and clss.codigo_Pai is null
+and cla.codigo_Pai = cls.codigo
+order by clss.nome;
+*/
